3341655665ccd3ac7b98222fc757e7b53b390788
romanssam <troubledyouthh@outlook.com>
2023-09-03 01:41:13 +0500

    controls done, add api config

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -2,16 +2,27 @@ import React, {useState, useEffect} from 'react';
 import Search from "./Search/Search.jsx";
 import {CustomSelect} from './CustomSelect/CustomSelect.jsx'
 
+const options = [
+    {value: 'Africa', label: 'Africa'}
+]
 
-const MyComponent = () => {
+
+const Controls = () => {
     const [value, setValue] = useState('')
-
+    const [region, setRegion] = useState('')
     return (
         <div style={{ padding: '2rem', display: 'flex', justifyContent: 'space-between', width: '100%' }}>
             <Search placeholder='Поиск по стране' value={value} onChange={(e) => setValue(e.target.value)} />
-            <CustomSelect/>
+            <CustomSelect
+                options={options}
+                placeholder='Фильтр по странам'
+                isClearable
+                isSearchable={false}
+                value={region}
+                onChange={setRegion}
+            />
         </div>
     );
 };
 
-export default MyComponent;
+export default Controls;
--- src/components/Controls/CustomSelect/CustomSelect.jsx
+++ src/components/Controls/CustomSelect/CustomSelect.jsx
@@ -17,8 +17,21 @@ export const CustomSelect = styled(Select).attrs({
         option: (provided, state) => ({
             ...provided,
             cursor: 'pointer',
-            color: state.isSelected ? 'var(--colors-bg)' : 'var(--colors-text)',
-            backgroundColor: state.isSelected ? 'var(--colors-bg)' : 'var(--colors-ui-base)'
+            color: 'var(--colors-text)',
+            backgroundColor: state.isSelected ? 'var(--colors-ui-base)' : 'var(--colors-bg)'
         })
     },
-})``;
+})`
+  width: 300px;
+  border-radius: var(--border-radius);
+  font-family: var(--family);
+  border: none;
+  
+  & > * {
+    box-shadow: var(--shadow);
+  }
+  
+  & > * {
+    color: var(--colors-text) !important;
+  }
+`;
