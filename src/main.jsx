f0bb907d11b0df1b203c871dcfed62929a147b6e
romanssam <troubledyouthh@outlook.com>
2023-09-07 22:51:29 +0500

    Revert "set routes like remix"
    This reverts commit 21e664c574192dc1ed5938a45e7b2023d353f577.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
 import {App} from './App.jsx'
-import './index.css';
-import HomePage from './pages/HomePage/HomePage.jsx'
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <HomePage/>
-    }
-])
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
