21e664c574192dc1ed5938a45e7b2023d353f577
romanssam <troubledyouthh@outlook.com>
2023-09-07 22:24:59 +0500

    set routes like remix

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,11 @@ import axios from 'axios';
 import './App.css'
 import Header from "./components/Header/Header.jsx";
 import Main from "./components/Main/Main.jsx";
-import Controls from "./components/Controls/Controls";
-import Countries from './components/Countries/Countries.jsx'
 
 export const App = () => {
-
     return (
     <>
-      <Header/>
-        <Main>
-            <Controls/>
-            <Suspense fallback={<h2>Lol</h2>}>
-                <Countries />
-            </Suspense>
-        </Main>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import {App} from './App.jsx'
-import './index.css'
+import './index.css';
+import HomePage from './pages/HomePage/HomePage.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage/>
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
